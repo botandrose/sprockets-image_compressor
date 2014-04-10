aaea90c7854eadd1101a1b13f643743241ba58a0
Micah Geisel <micah@botandrose.com>
2014-04-10 16:35:29 -0700

    refactor.

--- lib/sprockets/image_compressor/png_compressor.rb
+++ lib/sprockets/image_compressor/png_compressor.rb
@@ -8,25 +8,26 @@ module Sprockets
       end
 
       def compress(content)
-        compressed_png_data = content
+        compressed_png_data = ""
         Tempfile.open ["in_file", ".png"] do |in_file|
           in_file.binmode
           out_file_path = in_file.path + ".optimized.png"
           in_file.write content
           in_file.close
 
-          begin
-            out = `#{binary_path} #{in_file.path} #{out_file_path} 2>&1`
+          out = `#{binary_path} #{in_file.path} #{out_file_path} 2>&1`
 
-            File.open out_file_path, "rb" do |out_file|
-              compressed_png_data = out_file.read
-            end
-            File.unlink out_file_path
-          rescue Errno::ENOENT => e
-            # error during compression so out_file not found ...
+          File.open out_file_path, "rb" do |out_file|
+            compressed_png_data = out_file.read
           end
+          File.unlink out_file_path
         end
         compressed_png_data
+
+      rescue Errno::ENOENT => e
+        # error during compression so out_file not found
+        # return original content as a fallback
+        content
       end
     end
   end
