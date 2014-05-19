29d22a4667595b6eca9e0f02ff625a82b59b59a5
Jake Mitchell <jakemitchell@jake3p0.com>
2014-05-18 20:15:51 -0400

    Consolidated methods of reading compressed png/jpg file data using IO.binread; fixed jpg read error.

--- lib/sprockets/image_compressor/png_compressor.rb
+++ lib/sprockets/image_compressor/png_compressor.rb
@@ -16,10 +16,7 @@ module Sprockets
           in_file.close
 
           out = `#{binary_path} #{in_file.path} #{out_file_path} 2>&1`
-
-          File.open out_file_path, "rb" do |out_file|
-            compressed_png_data = out_file.read
-          end
+          compressed_png_data = IO.binread(out_file_path)
           File.unlink out_file_path
         end
         compressed_png_data
